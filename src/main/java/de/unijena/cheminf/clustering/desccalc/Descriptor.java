a917230b9bd09a30483eb44285a9736066555f6d
Achim Zielesny <achim.zielesny@googlemail.com>
2025-02-07 18:30:10 +0100

    Initial class and test classes

---
+++ src/main/java/de/unijena/cheminf/clustering/desccalc/Descriptor.java
@@ -0,0 +1,301 @@
+package de.unijena.cheminf.clustering.desccalc;
+
+import java.util.EnumMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// TODO Jonas: Implement IAtomContainer based methods instead of/in addition to SMILES based ones.
+
+/**
+ * Descriptor related calculations based on the CDK
+ * for the enrichment of data vectors for clustering
+ * Note: For adding a new descriptor goto "Add new descriptor here!"
+ *
+ * @author Jonas Schaub, Achim Zielesny
+ */
+public enum Descriptor {
+
+    //<editor-fold desc="Descriptor enumeration and initialization">
+    /*
+     * Molecular weight
+     */
+    MOLECULER_WEIGHT,
+    /*
+     * Wiener index
+     */
+    WIENER_INDEX;
+    // Add new descriptor here!
+
+    /*
+     * EnumMap that maps a descriptor to its number of calculated components
+     */
+    private static final EnumMap<Descriptor, Integer> descriptorToComponentNumberMap = new EnumMap<>(Descriptor.class);
+    static {
+        // MOLECULER_WEIGHT has 1 component
+        descriptorToComponentNumberMap.put(MOLECULER_WEIGHT, 1);
+        // WIENER_INDEX has 1 component
+        descriptorToComponentNumberMap.put(WIENER_INDEX, 1);
+        // Add new descriptor here!
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Private static final LOGGER">
+    /*
+     * Logger of this class
+     */
+    private static final Logger LOGGER = Logger.getLogger(Descriptor.class.getName());
+    //</editor-fold>
+
+    //<editor-fold desc="Public static methods">
+    /**
+     * Returns all available descriptors
+     *
+     * @return All available descriptors
+     */
+    public static Descriptor[] getAllDescriptors(){
+        return values();
+    }
+
+    /**
+     * Returns sum of number of calculated components of an array of defined descriptors
+     *
+     * @param aDescriptors Array of  descriptors
+     * @return Sum of number of calculated components of array of descriptors
+     * @throws IllegalArgumentException Thrown if an argument is illegal
+     * @throws Exception Thrown if number of components could not be evaluated: This should never happen.
+     */
+    public static int getNumberOfComponents(
+        Descriptor[] aDescriptors
+    ) throws IllegalArgumentException, Exception {
+        //<editor-fold desc="Checks">
+        if (aDescriptors == null || aDescriptors.length == 0) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.getNumberOfComponents: aDescriptors is null or has length 0."
+            );
+            throw new IllegalArgumentException("Descriptor.getNumberOfComponents: aDescriptor is null or has length 0.");
+        }
+        for (Descriptor tmpDescriptor : aDescriptors) {
+            if (tmpDescriptor == null) {
+                Descriptor.LOGGER.log(
+                    Level.SEVERE,
+                    "Descriptor.getNumberOfComponents: A descriptor in aDescriptors is null."
+                );
+                throw new IllegalArgumentException("Descriptor.getNumberOfComponents: A descriptor in aDescriptors is null.");
+            }
+        }
+        //</editor-fold>
+
+        try {
+            int tmpTotalNumberOfComponents = 0;
+            for (Descriptor tmpDescriptor : aDescriptors) {
+                tmpTotalNumberOfComponents += descriptorToComponentNumberMap.get(tmpDescriptor);
+            }
+            return tmpTotalNumberOfComponents;
+        } catch (Exception anException) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.getNumberOfComponents: Number of components could not be evaluated: This should never happen."
+            );
+            throw new Exception("Descriptor.getNumberOfComponents: An exception occurred: This should never happen.");
+        }
+    }
+
+    /**
+     * Sets calculated descriptor components in vectors of a aMatrix (that corresponds to aSmilesArray) beginning with
+     * aStartIndex.
+     *
+     * @param aDescriptors Array of descriptors to be calculated (IS NOT CHANGED)
+     * @param aSmilesArray Array of SMILES strings of molecules. Note: aSmilesArray[i] corresponds to aMatrix[i]. (IS
+     *                     NOT CHANGED)
+     * @param aMatrix Matrix of component vectors of molecules. Note: aMatrix[i] corresponds to aSmilesArray[i]. (MAY
+     *                BE CHANGED)
+     * @param aStartIndex Start index in a vector to be filled with calculated components of descriptors
+     * @return True: Operation was successful, false: Operation failed, i.e. at least one component in a descriptor
+     * calculation is NaN
+     * @throws IllegalArgumentException Thrown if an argument is illegal
+     * @throws Exception Thrown if fatal error occurs (this should never happen)
+     */
+    public static boolean setCalculatedDescriptorComponents (
+        Descriptor[] aDescriptors,
+        String[] aSmilesArray,
+        float[][] aMatrix,
+        int aStartIndex
+    ) throws IllegalArgumentException, Exception {
+        //<editor-fold desc="Checks">
+        if (aDescriptors == null || aDescriptors.length == 0) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.setCalculatedDescriptorComponents: aDescriptors is null or has length 0."
+            );
+            throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: aDescriptor is null or has length 0.");
+        }
+        for (Descriptor tmpDescriptor : aDescriptors) {
+            if (tmpDescriptor == null) {
+                Descriptor.LOGGER.log(
+                    Level.SEVERE,
+                    "Descriptor.setCalculatedDescriptorComponents: A descriptor in aDescriptors is null."
+                );
+                throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: A descriptor in aDescriptors is null.");
+            }
+        }
+        if (aSmilesArray == null || aSmilesArray.length == 0) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.setCalculatedDescriptorComponents: aSmilesArray is null or has length 0."
+            );
+            throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: aSmilesArray is null or has length 0.");
+        }
+        for (String tmpSmiles : aSmilesArray) {
+            if (tmpSmiles == null || tmpSmiles.isEmpty()) {
+                Descriptor.LOGGER.log(
+                    Level.SEVERE,
+                    "Descriptor.setCalculatedDescriptorComponents: A SMILES in aSmilesArray is null or empty."
+                );
+                throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: A SMILES in aSmilesArray is null or empty.");
+            }
+        }
+        if (aMatrix == null || aMatrix.length == 0) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.setCalculatedDescriptorComponents: aMatrix is null or has length 0."
+            );
+            throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: aMatrix is null or has length 0.");
+        }
+        if (aMatrix.length != aSmilesArray.length) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.setCalculatedDescriptorComponents: aMatrix and aSmilesArray must have the same length."
+            );
+            throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: aMatrix and aSmilesArray must have the same length.");
+        }
+        for (float[] tmpVector : aMatrix) {
+            if (tmpVector == null || tmpVector.length == 0) {
+                Descriptor.LOGGER.log(
+                    Level.SEVERE,
+                    "Descriptor.setCalculatedDescriptorComponents: A vector is null or has length 0."
+                );
+                throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: A vector is null or has length 0.");
+            }
+            if (aStartIndex >= tmpVector.length) {
+                Descriptor.LOGGER.log(
+                    Level.SEVERE,
+                    "Descriptor.setCalculatedDescriptorComponents: aStartIndex is illegal."
+                );
+                throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: aStartIndex is illegal.");
+            }
+            try {
+                if (aStartIndex + Descriptor.getNumberOfComponents(aDescriptors) > tmpVector.length) {
+                    Descriptor.LOGGER.log(
+                        Level.SEVERE,
+                        "Descriptor.setCalculatedDescriptorComponents: aStartIndex is illegal."
+                    );
+                    throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: aStartIndex is illegal.");
+                }
+            } catch (Exception anException) {
+                Descriptor.LOGGER.log(
+                    Level.SEVERE,
+                    "Descriptor.setCalculatedDescriptorComponents: aStartIndex is illegal."
+                );
+                throw new IllegalArgumentException("Descriptor.setCalculatedDescriptorComponents: aStartIndex is illegal.");
+            }
+        }
+        //</editor-fold>
+
+        try {
+            boolean tmpIsSuccessful = true;
+            for (int i = 0; i < aSmilesArray.length; i++) {
+                if (!Descriptor.setCalculatedDescriptorComponents(aDescriptors, aSmilesArray[i], aMatrix[i], aStartIndex)) {
+                    tmpIsSuccessful = false;
+                }
+            }
+            return tmpIsSuccessful;
+        } catch (Exception anException) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.setCalculatedDescriptorComponents: An exception occurred: This should never happen."
+            );
+            throw anException;
+        }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Private static methods">
+    /**
+     * Sets calculated descriptor components in aVector (that corresponds to aSmiles) beginning with aStartIndex
+     * Note: No checks are NOT performed. All necessary checks have already been made in public methods above.
+     *
+     * @param aDescriptors Array of descriptors to be calculated
+     * @param aSmiles SMILES of molecule
+     * @param aVector Component vector of molecule
+     * @param aStartIndex Start index in aVector to be filled with calculated components of descriptors
+     * @return True: Operation was successful, false: Operation failed, i.e. at least one component in a
+     * descriptor calculation is NaN
+     * @throws Exception Thrown if fatal error occurs (this should never happen)
+     */
+    private static boolean setCalculatedDescriptorComponents (
+            Descriptor[] aDescriptors,
+            String aSmiles,
+            float[] aVector,
+            int aStartIndex
+    ) throws Exception {
+        try {
+            boolean tmpIsSuccessful = true;
+            for (Descriptor tmpDescriptor : aDescriptors) {
+                if (!Descriptor.setComponentValues(tmpDescriptor, aSmiles, aVector, aStartIndex)) {
+                    tmpIsSuccessful = false;
+                }
+                aStartIndex += descriptorToComponentNumberMap.get(tmpDescriptor);
+            }
+            return tmpIsSuccessful;
+        } catch (Exception anException) {
+            Descriptor.LOGGER.log(
+                Level.SEVERE,
+                "Descriptor.setCalculatedDescriptorComponents: An exception occurred: This should never happen."
+            );
+            throw new Exception("Descriptor.setCalculatedDescriptorComponents: An exception occurred: This should never happen.");
+        }
+    }
+
+    /**
+     * Sets component values of aDescriptor for aSmiles in aVector beginning with aStartIndex.
+     * Note: No checks are NOT performed. All necessary checks have already been made in public methods above.
+     *
+     * @param aDescriptor Descriptor to be calculated
+     * @param aSmiles SMILES of molecule
+     * @param aVector Vector of molecule to be filled with calculated components of descriptors
+     * @param aStartIndex Start index in aVector to be filled with calculated components of descriptors
+     * @return True: Operation was successful, false: Operation failed, i.e. at least one component in a
+     * descriptor calculation is NaN
+     */
+    private static boolean setComponentValues(
+        Descriptor aDescriptor,
+        String aSmiles,
+        float[] aVector,
+        int aStartIndex
+    ) {
+        try {
+            switch (aDescriptor) {
+                case MOLECULER_WEIGHT:
+                    // TODO Jonas: Calculate components of descriptor for aSmiles with CDK and set aVector
+                    // Demo code:
+                    aVector[aStartIndex] = 500.0f;
+                    break;
+                case WIENER_INDEX:
+                    // TODO Jonas: Calculate components of descriptor for aSmiles with CDK and set aVector
+                    // Demo code:
+                    aVector[aStartIndex] = 70.0f;
+                    break;
+                // Add new descriptor here!
+            }
+            return true;
+        } catch (Exception anException) {
+            for (int i = aStartIndex; i < descriptorToComponentNumberMap.get(aDescriptor); i++) {
+                aVector[i] = Float.NaN;
+            }
+            return false;
+        }
+    }
+    //</editor-fold>
+
+}
---
+++ src/test/java/de/unijena/cheminf/clustering/desccalc/DescriptorTest.java
@@ -0,0 +1,84 @@
+package de.unijena.cheminf.clustering.desccalc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for descriptor
+ */
+class DescriptorTest {
+
+    /**
+     * Test method for descriptor MOLECULAR_WEIGHT
+     */
+    @Test
+    public void test_MOLECULAR_WEIGHT() {
+        // Ethanol
+        String tmpSmiles = "CC(=O)O";
+        String[] tmpSmilesArray = new String[] {tmpSmiles};
+        float[][] tmpMatrix = new float[][]
+            {
+                {0f}
+            };
+        int tmpStartIndex = 0;
+        Descriptor[] tmpDescriptors = new Descriptor[] {Descriptor.MOLECULER_WEIGHT};
+
+        try {
+            Assertions.assertEquals(1, Descriptor.getNumberOfComponents(tmpDescriptors));
+            Assertions.assertTrue(Descriptor.setCalculatedDescriptorComponents(tmpDescriptors, tmpSmilesArray, tmpMatrix, tmpStartIndex));
+            Assertions.assertEquals(500.0f, tmpMatrix[0][0]);
+        } catch (Exception anException) {
+            Assertions.fail();
+        }
+    }
+
+    /**
+     * Test method for descriptor WIENER_INDEX
+     */
+    @Test
+    public void test_WIENER_INDEX() {
+        // Ethanol
+        String tmpSmiles = "CC(=O)O";
+        String[] tmpSmilesArray = new String[] {tmpSmiles};
+        float[][] tmpMatrix = new float[][]
+                {
+                        {0f}
+                };
+        int tmpStartIndex = 0;
+        Descriptor[] tmpDescriptors = new Descriptor[] {Descriptor.WIENER_INDEX};
+
+        try {
+            Assertions.assertEquals(1, Descriptor.getNumberOfComponents(tmpDescriptors));
+            Assertions.assertTrue(Descriptor.setCalculatedDescriptorComponents(tmpDescriptors, tmpSmilesArray, tmpMatrix, tmpStartIndex));
+            Assertions.assertEquals(70.0f, tmpMatrix[0][0]);
+        } catch (Exception anException) {
+            Assertions.fail();
+        }
+    }
+
+    /**
+     * Test multiple descriptors
+     */
+    @Test
+    public void test_MultipleDescriptors() {
+        // Ethanol
+        String tmpSmiles = "CC(=O)O";
+        String[] tmpSmilesArray = new String[] {tmpSmiles};
+        int tmpStartIndex = 0;
+        float[][] tmpMatrix = new float[][]
+                {
+                        {0f, 0f}
+                };
+        Descriptor[] tmpDescriptors = new Descriptor[] {Descriptor.MOLECULER_WEIGHT, Descriptor.WIENER_INDEX};
+
+        try {
+            Assertions.assertEquals(2, Descriptor.getNumberOfComponents(tmpDescriptors));
+            Assertions.assertTrue(Descriptor.setCalculatedDescriptorComponents(tmpDescriptors, tmpSmilesArray, tmpMatrix, tmpStartIndex));
+            Assertions.assertEquals(500.0f, tmpMatrix[0][0]);
+            Assertions.assertEquals(70.0f, tmpMatrix[0][1]);
+        } catch (Exception anException) {
+            Assertions.fail();
+        }
+    }
+
+}
