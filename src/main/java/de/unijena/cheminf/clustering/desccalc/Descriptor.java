2893b37050562ac7a38842f901eb4b3d5792aa68
Manuel Schauer <105295641+ManuelSchauer@users.noreply.github.com>
2026-06-24 15:01:38 +0200

    spotless fix: removed unused import

--- src/main/java/de/unijena/cheminf/clustering/desccalc/Descriptor.java
+++ src/main/java/de/unijena/cheminf/clustering/desccalc/Descriptor.java
@@ -93,7 +93,6 @@ import org.openscience.cdk.qsar.result.IntegerArrayResult;
 import org.openscience.cdk.qsar.result.IntegerResult;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.smiles.SmilesParser;
-import org.openscience.cdk.tools.CDKHydrogenAdder;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 import org.openscience.cdk.tools.manipulator.HydrogenState;
 
